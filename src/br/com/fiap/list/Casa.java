a8fee89df376b102f293050b4fd89c76a8f77e78
oThinas <thiagomartinsprado@gmail.com>
2022-05-12 09:44:11 -0300

    Exercício "Casa

---
+++ src/br/com/fiap/list/Casa.java
@@ -0,0 +1,12 @@
+package br.com.fiap.list;
+
+public class Casa {
+    private double areaTotal;
+
+    public void setAreaTotal(double areaComodo) {
+        this.areaTotal += areaComodo;
+    }
+    public double getAreaTotal() {
+        return areaTotal;
+    }
+}
